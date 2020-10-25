b81aa410e0dc7b19003b024ce85114f97489e251
FaziletKosure <faziletkosure1@gmail.com>
2020-10-25 03:40:57 +0100

    add style

--- src/pages/PerfectNumber.js
+++ src/pages/PerfectNumber.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
+import {color} from 'react-native-reanimated';
 
 const PerfectNumber = () => {
   const [number, setNumber] = useState('');
@@ -24,16 +25,37 @@ const PerfectNumber = () => {
   }
   return (
     <View style={styles.container}>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 30,
+          fontWeight: 'bold',
+          color: 'white',
+          marginBottom: 25,
+        }}>
+        Check Perfect Number
+      </Text>
       <TextInput
         style={styles.textInput}
         value={number}
         onChangeText={(val) => setNumber(val)}
       />
-      <TouchableOpacity onPress={() => perfect_num(number)}>
+      <TouchableOpacity
+        style={{
+          borderRadius: 25,
+          margin: 5,
+          backgroundColor: '#005662',
+          width: '40%',
+        }}
+        onPress={() => perfect_num(number)}>
         <Text style={styles.text}>Check</Text>
       </TouchableOpacity>
-      {isPerfect && <Text>it is a perfect number</Text>}
-      {!isPerfect && <Text>it is not a perfect number</Text>}
+      {isPerfect && (
+        <Text style={styles.text_per}>it is a perfect number 😎</Text>
+      )}
+      {!isPerfect && (
+        <Text style={styles.text_per}>it is not a perfect number</Text>
+      )}
     </View>
   );
 };
@@ -46,11 +68,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    backgroundColor: '#b2ebf2',
+    backgroundColor: '#4fb3bf',
   },
   textInput: {
-    borderColor: 'black',
-    borderWidth: 1,
+    // borderColor: 'black',
+    // borderWidth: 1,
     borderRadius: 15,
     width: '90%',
     fontSize: 20,
@@ -60,5 +82,14 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 7,
+    color: 'white',
+    alignSelf: 'center',
+    padding: 5,
+  },
+  text_per: {
+    fontSize: 20,
+    color: 'white',
+    marginVertical: 10,
   },
 });
